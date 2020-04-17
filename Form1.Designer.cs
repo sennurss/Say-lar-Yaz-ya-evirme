e617fe11c4927ccd500251128d87a6de8d252be2
sennurss <sennur_berber@hotmail.com>
2020-04-17 17:23:50 +0300

    Sayıları Yazıya Çevirme

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Arrys
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxSayi = new System.Windows.Forms.TextBox();
+            this.buttonYaziyaCevir = new System.Windows.Forms.Button();
+            this.labelYazi = new System.Windows.Forms.Label();
+            this.buttonTemizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxSayi
+            // 
+            this.textBoxSayi.Location = new System.Drawing.Point(13, 53);
+            this.textBoxSayi.Name = "textBoxSayi";
+            this.textBoxSayi.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSayi.TabIndex = 0;
+            // 
+            // buttonYaziyaCevir
+            // 
+            this.buttonYaziyaCevir.Location = new System.Drawing.Point(12, 127);
+            this.buttonYaziyaCevir.Name = "buttonYaziyaCevir";
+            this.buttonYaziyaCevir.Size = new System.Drawing.Size(75, 23);
+            this.buttonYaziyaCevir.TabIndex = 1;
+            this.buttonYaziyaCevir.Text = "Yazıya Çevir";
+            this.buttonYaziyaCevir.UseVisualStyleBackColor = true;
+            this.buttonYaziyaCevir.Click += new System.EventHandler(this.buttonYaziyaCevir_Click);
+            // 
+            // labelYazi
+            // 
+            this.labelYazi.AutoSize = true;
+            this.labelYazi.Location = new System.Drawing.Point(12, 92);
+            this.labelYazi.Name = "labelYazi";
+            this.labelYazi.Size = new System.Drawing.Size(22, 13);
+            this.labelYazi.TabIndex = 2;
+            this.labelYazi.Text = ".....";
+            // 
+            // buttonTemizle
+            // 
+            this.buttonTemizle.Location = new System.Drawing.Point(117, 126);
+            this.buttonTemizle.Name = "buttonTemizle";
+            this.buttonTemizle.Size = new System.Drawing.Size(75, 23);
+            this.buttonTemizle.TabIndex = 3;
+            this.buttonTemizle.Text = "Temizle";
+            this.buttonTemizle.UseVisualStyleBackColor = true;
+            this.buttonTemizle.Click += new System.EventHandler(this.buttonTemizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(296, 298);
+            this.Controls.Add(this.buttonTemizle);
+            this.Controls.Add(this.labelYazi);
+            this.Controls.Add(this.buttonYaziyaCevir);
+            this.Controls.Add(this.textBoxSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSayi;
+        private System.Windows.Forms.Button buttonYaziyaCevir;
+        private System.Windows.Forms.Label labelYazi;
+        private System.Windows.Forms.Button buttonTemizle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Arrys
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            buttonTemizle.Enabled = false;
+        }
+
+        private void buttonYaziyaCevir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            string[] birler = {"","bir","iki","üç","dört","beş","altı","yedi","sekiz","dokuz" };
+            string[] onlar = { "", "on", "yirmi", "otuz", "kırk", "elli", "altmış", "yetmiş", "seksen", "doksan" };
+            string[] yuzler = { "", "yüz", "ikiyüz", "üçyüz", "dörtyüz", "beşyüz", "altıyüz", "yediyüz", "sekizyüz", "dokuzyüz" };
+            string[] binler = { "", "bin", "ikibin", "üçbin", "dörtbin", "beşbin", "altıbin", "yedibin", "sekizbin", "dokuzbin" };
+            string[] onbinler = { "", "onbin", "yirmibin", "otuzbin", "kırkbin", "ellibin", "altmışbin", "yetmişbin", "seksenbin", "doksanbin" };
+
+           
+                int sayi = Convert.ToInt32(textBoxSayi.Text);
+
+                if (sayi >= 1000000000)
+                {
+                    MessageBox.Show("Bu işlem sadece birler ile onbinler arasındaki sayıları kapsamaktadır");
+                }
+
+                int onbinlerbasamagi = sayi % 100000 / 10000;
+                int binlerbasamagi = sayi % 10000 / 1000;
+                int yuzlerbasamagi = sayi % 1000 / 100;
+                int onlarbasamagi = sayi % 100 / 10;
+                int birlerbasamagi = sayi % 10;
+
+                string yazi = onbinler[onbinlerbasamagi] + binler[binlerbasamagi] + yuzler[yuzlerbasamagi] + onlar[onlarbasamagi] + birler[birlerbasamagi];
+                labelYazi.Text = yazi;
+                
+                buttonTemizle.Enabled = true;
+                buttonYaziyaCevir.Enabled = true;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Lütfen sayısal deger giriniz");
+            }
+            
+
+            
+
+            
+
+        }
+
+        private void buttonTemizle_Click(object sender, EventArgs e)
+        {
+            buttonTemizle.Enabled = false;
+            labelYazi.Text = null;
+            textBoxSayi.Text = null;
+        }
+    }
+}
